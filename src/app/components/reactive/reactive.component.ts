1ac3b0311cb31b1a5a45197a3bafc385cb377e55
Jorge <jorgecampoy81@gmail.com>
2019-04-11 15:36:56 +0200

    Added form array

--- src/app/components/reactive/reactive.component.ts
+++ src/app/components/reactive/reactive.component.ts
@@ -1,52 +1,61 @@
-import {Component, OnInit} from "@angular/core"
-import {FormGroup, FormControl, Validators} from "@angular/forms"
+import {Component, OnInit} from '@angular/core'
+import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms'
 
 @Component({
-  selector: "app-reactive",
-  templateUrl: "./reactive.component.html",
-  styleUrls: ["./reactive.component.sass"]
+  selector: 'app-reactive',
+  templateUrl: './reactive.component.html',
+  styleUrls: ['./reactive.component.sass']
 })
 export class ReactiveComponent implements OnInit {
-  forma: FormGroup;
+  forma: FormGroup
   usuario = {
     nombreCompleto: {
       nombre: 'Jorge',
       apellido: 'Campoy'
     },
-    correo: 'jorgecampoy81@gmail.com'
+    correo: 'jorgecampoy81@gmail.com',
+    pasatiempos: ['Correr', 'Leer', 'Youtube']
   }
 
   constructor() {
     this.forma = new FormGroup({
       nombreCompleto: new FormGroup({
         // nombre: new FormControl(this.usuario.nombreCompleto.nombre, [
-        nombre: new FormControl( '' , [
+        nombre: new FormControl('', [
           Validators.required,
           Validators.minLength(3)
         ]),
-        apellido: new FormControl("", [
+        apellido: new FormControl('', [
           Validators.required,
           Validators.minLength(3)
         ])
       }),
-      correo: new FormControl("jorgecampoy81@gmail.com", [
+      correo: new FormControl('jorgecampoy81@gmail.com', [
         Validators.required,
-        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$")
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')
+      ]),
+      pasatiempos: new FormArray([
+        new FormControl('Correr', Validators.required),
       ])
     })
+    this.forma.setValue(this.usuario)
+  }
 
-    this.forma.setValue( this.usuario )
+  agregarPasatiempos() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('Correr', Validators.required)
+    );
   }
 
   guardarCambios() {
-    console.log(this.forma)
-    this.forma.reset({
-      nombreCompleto: {
-      nombre: '',
-      apellido: ''
-    },
-    correo: ''}
-    )
+    console.log(this.forma.value)
+    // this.forma.reset({
+    //   nombreCompleto: {
+    //     nombre: '',
+    //     apellido: ''
+    //   },
+    //   correo: ''
+    // })
   }
 
   ngOnInit() {}
